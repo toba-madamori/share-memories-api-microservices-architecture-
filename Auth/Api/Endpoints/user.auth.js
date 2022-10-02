f143b5cc223d99b868627239b9f29964bf7d3881
Toba <tobajamezmadamori@gmail.com>
2022-10-02 13:16:16 +0100

    modified signup api

--- Auth/Api/Endpoints/user.auth.js
+++ Auth/Api/Endpoints/user.auth.js
@@ -2,14 +2,16 @@ const UserService = require('../../Services/user.auth')
 const { StatusCodes } = require('http-status-codes')
 const validator = require('express-joi-validation').createValidator({})
 const { signupSchema } = require('../../Utils/validators')
+const upload = require('../../Utils/multer')
 
 module.exports = (app) => {
     const service = new UserService()
 
-    app.post('/signup', validator.body(signupSchema), async (req, res) => {
+    app.post('/signup', upload.single('avatar'), validator.body(signupSchema), async (req, res) => {
         const { name, email, password } = req.body
+        const avatar = req.file
 
-        await service.SignUp({ name, email, password })
+        await service.SignUp({ name, email, password, avatar })
         res.status(StatusCodes.CREATED).json({ status: 'success', msg: 'verification link sent to email' })
     })
 
