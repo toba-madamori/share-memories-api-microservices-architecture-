9237321bb879901779a8f5a71e0876827a122c8d
Toba <tobajamezmadamori@gmail.com>
2022-10-02 13:14:50 +0100

    updated multer utility

--- Auth/Utils/multer.js
+++ Auth/Utils/multer.js
@@ -1,14 +1,8 @@
 const multer = require('multer')
 const path = require('path')
-const maxSize = 5 * 1000 * 1000
+const maxSize = 1 * 1000 * 1000
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        if (file.fieldname === 'avatar') {
-            const path = process.cwd() + '/uploads/avatar'
-            cb(null, path)
-        }
-    },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
         cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname)
