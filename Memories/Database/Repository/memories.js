c88a0ff0589a8cae2d5448ef848295b5558909a1
Toba <tobajamezmadamori@gmail.com>
2022-10-08 14:47:10 +0100

    added create memory repository call

--- Memories/Database/Repository/memories.js
+++ Memories/Database/Repository/memories.js
@@ -3,7 +3,18 @@ const MemoryModel = require('../Models/memories')
 
 // Dealing with data base operations
 class MemoryRepository {
+    async createMemory ({ userid, memory, title, tags, cloudinary_id }) {
+        const mem = new MemoryModel({
+            userid,
+            memory,
+            title,
+            tags,
+            cloudinary_id
+        })
 
+        const newMemory = await mem.save()
+        return newMemory
+    }
 }
 
 module.exports = MemoryRepository
