34618f55ed069954e8e47ce1d50b3dd9f1d45c81
Toba <tobajamezmadamori@gmail.com>
2022-10-08 14:46:35 +0100

    added create memory service

--- Memories/Services/memories.js
+++ Memories/Services/memories.js
@@ -1,10 +1,27 @@
 /* eslint-disable camelcase */
 const { MemoryRepository } = require('../Database')
+const cloudinary = require('../Utils/cloudinary')
+const { BadRequestError } = require('../Errors')
 
 // All Business logic will be here
 class MemoryService {
     constructor () {
         this.repository = new MemoryRepository()
+    }
+
+    async createMemory (input) {
+        const { userid, title, tags, image } = input
+        let result = {}
+
+        if (!image) throw new BadRequestError('cannot create memories without an image')
+        result = await cloudinary.uploader.upload(image.path, { folder: 'memory_upload' })
+
+        const memory = result.secure_url
+        const cloudinary_id = result.public_id
+
+        const newMemory = await this.repository.createMemory({ userid, memory, title, tags, cloudinary_id })
+
+        return newMemory
     }
 }
 
