afefc15e026e5f1f6087a8a555fd564fb0ac3837
Toba <tobajamezmadamori@gmail.com>
2022-10-15 12:14:24 +0100

    added updated comment repository call

--- Reactions/Database/Repository/comments.js
+++ Reactions/Database/Repository/comments.js
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 const CommentModel = require('../Models/comments')
+const { NotFoundError } = require('../../Errors')
 
 // Dealing with data base operations
 class CommentRepository {
@@ -12,6 +13,19 @@ class CommentRepository {
 
         const newComment = await comm.save()
         return newComment
+    }
+
+    async findComment ({ _id }) {
+        const comment = await CommentModel.findById(_id)
+
+        if (!comment) throw new NotFoundError('sorry, this comment does not exist')
+
+        return comment
+    }
+
+    async updateComment ({ comment, userid, commentid }) {
+        const updatedComment = await CommentModel.findOneAndUpdate({ _id: commentid, userid }, { comment, edited: true }, { new: true })
+        return updatedComment
     }
 }
 
