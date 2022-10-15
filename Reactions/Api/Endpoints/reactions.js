852075301dba15f78fb112c77591ff85e97b2d9a
Toba <tobajamezmadamori@gmail.com>
2022-10-15 11:26:32 +0100

    added create new comment endpoint

---
+++ Reactions/Api/Endpoints/reactions.js
@@ -0,0 +1,23 @@
+/* eslint-disable camelcase */
+const ReactionsService = require('../../Services/reactions')
+const { StatusCodes } = require('http-status-codes')
+const { newCommentSchema, idSchema } = require('../../Utils/validators')
+const validator = require('express-joi-validation').createValidator({})
+const authMiddleware = require('../Middleware/authentication')
+
+module.exports = (app) => {
+    const service = new ReactionsService()
+
+    app.post('/comment/new/:id', authMiddleware, validator.params(idSchema), validator.body(newCommentSchema), async (req, res) => {
+        let { comment } = req.body
+        const { id: memoryid } = req.params
+        const { userID: userid } = req.user
+
+        comment = await service.createComment({ comment, userid, memoryid })
+        res.status(StatusCodes.CREATED).json({ status: 'success', comment })
+    })
+
+    app.get('/whoami', (req, res, next) => {
+        return res.status(StatusCodes.OK).json({ msg: '/reactions : I am Reactions Service' })
+    })
+}
