1ebf35ad34d06c0e4d43a382581a65769f2b6c4d
Toba <tobajamezmadamori@gmail.com>
2022-10-15 17:29:32 +0100

    added get memory endpoint

--- Memories/Api/Endpoints/memories.js
+++ Memories/Api/Endpoints/memories.js
@@ -1,7 +1,8 @@
 /* eslint-disable camelcase */
 const MemoryService = require('../../Services/memories')
 const { StatusCodes } = require('http-status-codes')
-const { newMemorySchema } = require('../../Utils/validators')
+const { newMemorySchema, idSchema } = require('../../Utils/validators')
+const { publishReactionEvent } = require('../../Utils/events')
 const validator = require('express-joi-validation').createValidator({})
 const upload = require('../../Utils/multer')
 const authMiddleware = require('../Middleware/authentication')
@@ -16,6 +17,16 @@ module.exports = (app) => {
 
         const memory = await service.createMemory({ userid, title, tags, image })
         res.status(StatusCodes.CREATED).json({ status: 'success', memory })
+    })
+
+    app.get('/:id', authMiddleware, validator.params(idSchema), async (req, res) => {
+        const { id: memoryid } = req.params
+
+        const memory = await service.getMemory({ memoryid })
+        // call to reactions-service for memory-comments
+        const response = await publishReactionEvent({ event: 'GET_COMMENTS', data: { memoryid } })
+
+        res.status(StatusCodes.OK).json({ status: 'success', memory, comments: response.data.data })
     })
 
     app.get('/whoami', (req, res, next) => {
