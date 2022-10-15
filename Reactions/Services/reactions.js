e52588e521a5aa8f2e32b0dcb677c5be14cfb7d1
Toba <tobajamezmadamori@gmail.com>
2022-10-15 12:39:15 +0100

    added get comments service

--- Reactions/Services/reactions.js
+++ Reactions/Services/reactions.js
@@ -28,6 +28,12 @@ class ReactionsService {
         const { _id, userid } = input
         return await this.commentRepository.deleteComment({ _id, userid })
     }
+
+    async getComments (input) {
+        const { memoryid } = input
+        const comments = await this.commentRepository.getComments({ memoryid })
+        return comments
+    }
 }
 
 module.exports = ReactionsService
