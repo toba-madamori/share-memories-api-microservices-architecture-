d6c0ccd50a405667d1dc4177d9bf6adf6b0a9c7b
Toba <tobajamezmadamori@gmail.com>
2022-10-29 15:35:20 +0100

    fixed error-handler middleware

--- Memories/Api/Middleware/errorHandler.js
+++ Memories/Api/Middleware/errorHandler.js
@@ -1,7 +1,6 @@
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    console.log(err)
     const customError = {
         statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
         msg: err.message || 'something went wrong, please try again later...'
