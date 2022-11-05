60950031cd91edb4de811f1403b05039abcbb902
Toba <tobajamezmadamori@gmail.com>
2022-11-05 17:18:43 +0100

    service setup

--- Users/Services/users.js
+++ Users/Services/users.js
@@ -1,6 +1,6 @@
+/* eslint-disable no-useless-constructor */
 /* eslint-disable camelcase */
 const logger = require('../Utils/logger')
-const { publishMemoryEvent, publishAuthEvent, publishReactionEvent } = require('../Utils/events')
 
 // All Business logic will be here
 class UserService {
@@ -13,7 +13,7 @@ class UserService {
 
         const { event, data } = payload
 
-        const { } = data
+        const {} = data
 
         switch (event) {
         case 'TEST':
