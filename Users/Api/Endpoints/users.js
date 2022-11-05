fb0b00e98aa813c9b93097ccfc86c84169ff2934
Toba <tobajamezmadamori@gmail.com>
2022-11-05 17:18:16 +0100

    added get user endpoint

--- Users/Api/Endpoints/users.js
+++ Users/Api/Endpoints/users.js
@@ -1,12 +1,23 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable camelcase */
 const UserService = require('../../Services/users')
 const { StatusCodes } = require('http-status-codes')
 const { idSchema } = require('../../Utils/validators')
 const validator = require('express-joi-validation').createValidator({})
 const authMiddleware = require('../Middleware/authentication')
+const { publishAuthEvent } = require('../../Utils/events')
 
 module.exports = (app) => {
     const service = new UserService()
+
+    app.get('/profile/find', authMiddleware, async (req, res) => {
+        const { userID: userid } = req.user
+
+        // call to auth-service for user profile
+        const response = await publishAuthEvent({ event: 'GET_USER', data: { userid } })
+
+        res.status(StatusCodes.OK).json({ status: 'success', user: response.data.data })
+    })
 
     app.get('/whoami', (req, res, next) => {
         return res.status(StatusCodes.OK).json({ msg: '/users : I am User Service' })
