af381de1012824b454508b3d44dfda5c4843a215
Toba <tobajamezmadamori@gmail.com>
2022-11-05 19:31:37 +0100

    added multer utility, modified validators

--- Users/Utils/validators.js
+++ Users/Utils/validators.js
@@ -1,9 +1,19 @@
 const Joi = require('joi')
 
+const name = Joi.string().trim(true)
+const email = Joi.string().email().trim(true)
+
+const updateUserSchema = Joi.object().keys({
+    name,
+    email,
+    status: Joi.string().trim(true)
+})
+
 const idSchema = Joi.object().keys({
     id: Joi.string().hex().length(24).pattern(/^[0-9a-fA-F]{24}$/).required()
 })
 
 module.exports = {
-    idSchema
+    idSchema,
+    updateUserSchema
 }
